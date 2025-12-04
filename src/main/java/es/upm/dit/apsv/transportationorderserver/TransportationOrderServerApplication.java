b9d759f8697da44ba5df96243d3bc029d18deb3f
MeryPMG <your_email@example.com>
2025-12-04 03:02:48 +0100

    first commit

---
+++ src/main/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderServerApplication.java
@@ -0,0 +1,15 @@
+package es.upm.dit.apsv.transportationorderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import es.upm.dit.apsv.transportationorderserver.repository.TransportationOrderRepository;
+
+@SpringBootApplication
+public class TransportationOrderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportationOrderServerApplication.class, args);
+	}
+
+}
